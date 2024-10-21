af280d466f645437a38589fef2fe2753e247facc
kosssshhhh <kosukwhon@gmail.com>
2024-10-21 19:37:03 +0900

    [ 🐛 | Fix ] 가격대 별 상품 수 그래프 x축 수정

--- client/src/pages/home/_utils/formatPriceRange.ts
+++ client/src/pages/home/_utils/formatPriceRange.ts
@@ -1,4 +1,11 @@
 export const formatPriceRange = (range: string) => {
+	// 범위를 '-'로 구분하여 나누고, max 값이 없는 경우 처리
 	const [min, max] = range.split('-').map((num) => parseInt(num, 10).toLocaleString());
-	return `${min}~${max}`;
+
+	// max 값이 없는 경우(즉, 10000000+)
+	if (!max) {
+		return `${min}원 +`; // min 값 뒤에 '원 +' 붙여서 출력
+	}
+
+	return `${min}~${max}원`;
 };
